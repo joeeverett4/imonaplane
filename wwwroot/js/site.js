9062b27b987c05d8f6b7305360d335446f419c6c
joeeverett4 <joe.everett34@gmail.com>
2023-08-08 15:27:06 +0100

    basic flight search

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -4,6 +4,17 @@
     var submitBtn = document.getElementById('submitBtn');
     var resultContainer = document.getElementById('result');
 
+    function parseDuration(duration) {
+        const matches = duration.match(/PT(\d+)H(\d+)M/);
+        if (matches) {
+            const hours = parseInt(matches[1]);
+            const minutes = parseInt(matches[2]);
+            return `${hours} hours and ${minutes} minutes`;
+        }
+        return 'Invalid duration format';
+    }
+
+
     submitBtn.addEventListener('click', function () {
         var requestData = {
             Slices: [{
@@ -21,6 +32,44 @@
         })
             .then(response => response.json())
             .then(data => {
+
+                let offers = data.data.offers;
+
+                let container = document.createElement('div');
+                container.id = 'offer-container';
+
+                offers.forEach(offer => {
+                    let html = `
+             <div class = "booking-list-item">
+               <div class = "booking-list-item-inner">
+                <div class="booking-list-top">
+                                        <div class="flight-airway">
+                                            <div class="flight-logo">
+                                               
+                                                <h5 class="title">${offer.owner.name}</h5>
+                                            </div>
+                                            <span>Operated by ${offer.owner.name}</span>
+                                        </div>
+                                        <ul class="flight-info">
+                                            <li>${offer.slices[0].segments[0].departing_at}</li>
+                                            <li class="time"><span>12: 55</span>DAC</li>
+                                            <li>${parseDuration(offer.slices[0].duration)}<span>2 Stops</span></li>
+                                        </ul>
+                                        <div class="flight-price">
+                                            <h4 class="title">GB£${offer.base_amount}</h4>
+                                            <a href="booking-details.html" class="btn">Select <i class="flaticon-flight-1"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                 `;
+                   container.insertAdjacentHTML('beforeend', html); // Append the HTML to the container
+                })
+
+                resultContainer.appendChild(container);
+
                 console.log(data)
             })
             .catch(error => {
